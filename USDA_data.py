30c9190a8c355933986a3a89b20244cee33c4f8d
Grace Li <novo@Graces-MacBook-Pro.local>
2022-01-05 23:29:15 -0500

    save images and excels in new folders

--- USDA_data.py
+++ USDA_data.py
@@ -1,9 +1,9 @@
-# Import libraries
 import matplotlib.pyplot as plt
 import pandas as pd
+import os
  
 # Read input data into dataframe df
-df = pd.read_csv('USDA_data.csv')
+df = pd.read_csv('USDA_data.csv', low_memory=False)
 
 # Part 2: Save all unique Mission Areas and Agency IDs into set tags
 tags = set()
@@ -11,6 +11,9 @@ tags = set()
 IDColumns = ['Asset - Custom Tags - Copy.2', 'Asset - Custom Tags - Copy.3', 'Asset - Custom Tags - Copy.4', 'Asset - Custom Tags - Copy.5']
 for column in IDColumns:
     tags.update(df[column].dropna().unique())
+# Make folders to store figures and data
+os.mkdir("./data")
+os.mkdir("./figures")
 
 # Part 3: Iterate through tags and create a 'tag_indicator' to signal if the data is within the tag's domain
 for tag in tags:
@@ -19,11 +22,10 @@ for tag in tags:
         df.loc[df[column] == tag, 'tag_indicator'] = True
     # Create a df_usage dataframe grouped by the 'Name' and 'Usage' of every 'tag' and export as a new 'tag_usage' excel
     df_usage = df.loc[df['tag_indicator']==True].groupby(['Name', 'Usage']).size().reset_index()
+    # Pivot the dataframe to display the software usage by usage levels
     df_usage.rename(columns={df_usage.columns[2]: "Values" }, inplace = True)
     df_usage = df_usage.pivot(index='Name', columns='Usage', values="Values")
-    df_usage.to_excel(tag + "_usage.xlsx")
-
-    # Part 4: Visualize usage data with grouped bar graphs
-    df_usage.plot(kind="bar")
-    plt.show()
-
+    df_usage.to_excel("./data/" + tag + "_usage.xlsx")
+    # Create bar grahs and save as PNGs
+    fig = df_usage.plot(kind="bar")
+    fig.figure.savefig("./figures/" + tag + "_image.png")
