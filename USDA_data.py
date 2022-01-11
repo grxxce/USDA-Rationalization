a1f1f61a60be8ba8e6b30821bb7f9f8cd70bc69d
Grace Li <novo@Graces-MacBook-Pro.local>
2022-01-11 11:35:23 -0500

    updated figures and groupby

--- USDA_data.py
+++ USDA_data.py
@@ -1,31 +1,39 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import os
- 
+
 # Read input data into dataframe df
 df = pd.read_csv('USDA_data.csv', low_memory=False)
 
-# Part 2: Save all unique Mission Areas and Agency IDs into set tags
+# Save all unique Mission Areas and Agency IDs into set tags
 tags = set()
-# Indicate all columns where tag data may be located and save unique tags into a set 'tags'
-IDColumns = ['Asset - Custom Tags - Copy.2', 'Asset - Custom Tags - Copy.3', 'Asset - Custom Tags - Copy.4', 'Asset - Custom Tags - Copy.5']
+# Indicate all columns containing tags and save unique into a set
+IDColumns = ['Asset - Custom Tags - Copy.2',
+             'Asset - Custom Tags - Copy.3',
+             'Asset - Custom Tags - Copy.4',
+             'Asset - Custom Tags - Copy.5'
+             ]
 for column in IDColumns:
     tags.update(df[column].dropna().unique())
 # Make folders to store figures and data
-os.mkdir("./data")
-os.mkdir("./figures")
+os.makedirs('./data', exist_ok=True)
+os.makedirs('./figures', exist_ok=True)
 
-# Part 3: Iterate through tags and create a 'tag_indicator' to signal if the data is within the tag's domain
+# Iterate through tags and use'tag_indicator' to signal data is within domain
 for tag in tags:
     df['tag_indicator'] = False
     for column in IDColumns:
         df.loc[df[column] == tag, 'tag_indicator'] = True
-    # Create a df_usage dataframe grouped by the 'Name' and 'Usage' of every 'tag' and export as a new 'tag_usage' excel
-    df_usage = df.loc[df['tag_indicator']==True].groupby(['Name', 'Usage']).size().reset_index()
+    # Group df_usage tags by descending usage and export as a new excel
+    df_usage = df.loc[df['tag_indicator'] == True].groupby(
+        ['Name', 'Usage']).size().reset_index()
     # Pivot the dataframe to display the software usage by usage levels
-    df_usage.rename(columns={df_usage.columns[2]: "Values" }, inplace = True)
-    df_usage = df_usage.pivot(index='Name', columns='Usage', values="Values")
-    df_usage.to_excel("./data/" + tag + "_usage.xlsx")
-    # Create bar grahs and save as PNGs
-    fig = df_usage.plot(kind="bar")
-    fig.figure.savefig("./figures/" + tag + "_image.png")
+    df_usage.rename(columns={df_usage.columns[2]: 'Values'}, inplace=True)
+    df_usage = df_usage.pivot(index='Name', columns='Usage', values='Values')
+    new_index = ['Baselining', 'Usage not detected', 'Normal', 'High']
+    df_usage = df_usage.reindex(columns=new_index)
+    df_usage.to_excel('./data/' + tag + '_usage.xlsx')
+    # Create bar graphs and save as PNGs
+    fig = df_usage.plot(kind='bar', rot=45, figsize=(20, 30))
+    plt.close()
+    fig.figure.savefig('./figures/' + tag + '_bar.png')
