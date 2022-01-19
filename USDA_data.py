ec64c5b75f49941dd99db324eb44bdcf6c372253
Luke Sorensen <lukie176mc@gmail.com>
2022-01-19 14:09:05 -0700

    Implement pie charts

--- USDA_data.py
+++ USDA_data.py
@@ -2,8 +2,10 @@ import matplotlib.pyplot as plt
 import os
 import pandas as pd
 
-# Read input data as DataFrame df
-df = pd.read_excel('USDA_data.xlsx')
+print('Importing Tanium dataset')
+
+# Read Tanium data as DataFrame df
+df = pd.read_excel('tanium.xlsx')
 
 # Clean data by removing baselining and duplicates
 df = df[df['Usage'] != 'Baselining'].drop_duplicates()
@@ -17,6 +19,14 @@ id_columns = [
     'Asset - Custom Tags - Copy.3',
     'Asset - Custom Tags - Copy.4',
     'Asset - Custom Tags - Copy.5'
+    # 'Asset - Custom Tags.2.1',
+    # 'Asset - Custom Tags.2.2.1',
+    # 'Asset - Custom Tags.2.2.2.1',
+    # 'Asset - Custom Tags.2.2.2.2.1',
+    # 'Asset - Custom Tags.2.2.2.2.2.1',
+    # 'Asset - Custom Tags.2.2.2.2.2.2.1',
+    # 'Asset - Custom Tags.2.2.2.2.2.2.2.1',
+    # 'Asset - Custom Tags.2.2.2.2.2.2.2.2.2.1'
 ]
 
 # Save all unique Mission Areas and Agency IDs into Set tags
@@ -28,77 +38,69 @@ for column in id_columns:
 os.makedirs('./data', exist_ok=True)
 os.makedirs('./figures', exist_ok=True)
 
-# Iterate through tags and create new df for every region
+# Iterate through tags and create DataFrames and visualizations
 for tag in tags:
-    # 'tag_indicator' signals if the data has the given tag
+    print('Processing tag: ' + tag)
+
+    # 'tag_indicator' signals if a row has the given tag
     df['tag_indicator'] = False
     for column in id_columns:
         df.loc[df[column] == tag, 'tag_indicator'] = True
 
-    # Group df by tags by descending usage and export as a new excel
+    # Group df by tags and usage
     df_tag = df.loc[df['tag_indicator']].groupby(['Name', 'Usage'])\
         .size().reset_index()
     df_tag.rename(columns={df_tag.columns[2]: 'Values'}, inplace=True)
-    # TO DO: Reindex 'Usage' to ascending order
 
-    # Set the right axes
-    softwares = set()
-    softwares.update(df_tag['Name'].dropna().unique())
-    softwares = list(softwares)
-    colors = {'High': 'g', 'Normal': 'y',
-              'Limited': 'r', 'Usage not detected': 'k'}
+    # Get usages and values for each software, required for visualizations
+    softwares = list(df_tag['Name'].dropna().unique())
+    usages, values = [], []
+    for software in softwares:
+        software_mask = df_tag['Name'] == software
+        usages.append(df_tag.loc[software_mask, 'Usage'])
+        values.append(df_tag.loc[software_mask, 'Values'])
+    colors = {
+        'High': 'green', 
+        'Normal': 'yellow', 
+        'Limited': 'orange', 
+        'Usage not detected': 'red'
+    }
 
-    # Generate the correct number of subplots
-    numRows = len(softwares) // 3
-    if len(softwares) < 3 and len(softwares) > 0:
-        numRows = 1
-    fig, ax = plt.subplots(nrows=numRows, ncols=3,
-                           squeeze=False, figsize=(8, ((len(softwares) // 3) + 1) * 3))
+    if len(softwares) > 0:
+        # Generate figure with appropriate grid of subplots
+        # Divides number of applications by 3 and rounds up
+        numRows = len(softwares) // 3 + (len(softwares) % 3 > 0)
+        fig, axs = plt.subplots(nrows=numRows, ncols=3,
+                                squeeze=False, figsize=(8, numRows * 3))
+        axs = axs.flat
 
-    if len(softwares) != 0:
-        # Create bar graphs and export as pngs
-        num = 0
-        i = 0
-        while (i < (len(softwares) // 3) or (i == 0)):
-            j = 0
-            while j < 3 and num < len(softwares):
-                usage = df_tag.loc[df_tag['Name'] == softwares[num], 'Usage']
-                value = df_tag.loc[df_tag['Name'] == softwares[num], 'Values']
-                ax[i, j].bar(usage, value, color=[colors[k] for k in usage])
-                ax[i, j].set_title(softwares[num], fontsize=10)
-                ax[i, j].tick_params(axis='x', labelrotation=30, labelsize=8)
-                ax[i, j].set_xlabel('Usage', fontsize=8)
-                ax[i, j].set_ylabel('Frequency', fontsize=8)
-                num += 1
-                j += 1
-            i += 1
+        # Remove all extra subplots
+        for ax in axs[len(softwares):]:
+            ax.remove()
+
+        # Set shared visualization features
         fig.suptitle(tag + ' usage by software', fontsize=12)
-        plt.tight_layout()
-        plt.subplots_adjust(wspace=0.5, hspace=2)
-        plt.savefig('./figures/' + tag + '_bar.png')
-        plt.close()
+        fig.subplots_adjust(wspace=1, hspace=1)
 
-        # Create pie charts and export as pngs
-        num = 0
-        i = 0
-        while (i < (len(softwares) // 3) or (i == 0)):
-            j = 0
-            while j < 3 and num < len(softwares):
-                usage = df_tag.loc[df_tag['Name'] == softwares[num], 'Usage']
-                value = df_tag.loc[df_tag['Name'] == softwares[num], 'Values']
-                ax[i, j].set_title(softwares[num], fontsize=10)
-                ax[i, j].set_xlabel('Usage', fontsize=8)
-                num += 1
-                j += 1
-            i += 1
-        # Label each plot accordingly
-        fig.suptitle(tag + ' usage by software', fontsize=12)
-        plt.tight_layout()
-        plt.subplots_adjust(wspace=0.5, hspace=2)
-        plt.savefig('./figures/' + tag + '_pie.png')
-        plt.close()
+        # Create bar charts and export as PNGs
+        for i in range(len(softwares)):
+            axs[i].set_title(softwares[i], fontsize=10)
+            axs[i].bar(usages[i], values[i], color=[colors[k] for k in usages[i]])
+            axs[i].tick_params(axis='x', labelrotation=30, labelsize=8)
+            axs[i].set_xlabel('Usage', fontsize=8)
+            axs[i].set_ylabel('Frequency', fontsize=8)
+        fig.savefig('./figures/' + tag + '_bar.png')
 
-    # Pivot the dataframe to display the software usage by usage levels
+        # Create pie charts and export as PNGs
+        for i in range(len(softwares)):
+            axs[i].clear()
+            axs[i].set_title(softwares[i], fontsize=10)
+            axs[i].pie(values[i], colors=[colors[k] for k in usages[i]])
+            axs[i].axis('Equal')
+        fig.savefig('./figures/' + tag + '_pie.png')
+        plt.close()
+    
+    # Pivot the DataFrame to display the software usage by usage levels
     df_tag = df_tag.pivot(index='Name', columns='Usage', values='Values')
     new_index = ['Usage not detected', 'Limited', 'Normal', 'High']
     df_tag = df_tag.reindex(columns=new_index)
