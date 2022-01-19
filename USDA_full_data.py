0851f528267dbd85402b866aee3fb97b33fd2e3b
Grace Li <novo@Graces-MacBook-Pro.local>
2022-01-19 18:11:06 -0500

    generates disjoint reports

--- USDA_full_data.py
+++ USDA_full_data.py
@@ -5,8 +5,8 @@ import os
 os.makedirs('./data', exist_ok=True)
 
 # Read Tanium input data as DataFrame df
-df_t = pd.read_excel('Tanium_data.xlsx')
-df_s = pd.read_excel('SCCM_data.xlsx')
+df_t = pd.read_excel('Tanium_data_sample.xlsx')
+df_s = pd.read_excel('SCCM_data_sample.xlsx')
 
 # # Clean data by removing baselining and duplicates
 df_t = df_t[df_t['Usage'] != 'Baselining'].drop_duplicates()
@@ -38,29 +38,35 @@ df_t_sub = df_t[tCols]
 
 df_s_sub = df_s[['Encrypted Workstation Name', 'Agency', 'OS']]
 
+df_t_sub.to_excel('./data/tanium_sub.xlsx')
+df_s_sub.to_excel('./data/sccm_sub.xlsx')
+
 # Merge dataframes on workstation and index with workstation
-df_workShared = df_t_sub.merge(
-    df_s_sub, on='Encrypted Workstation Name', how='inner', indicator=True)
-df_workShared.set_index('Encrypted Workstation Name')
+# df_workShared = df_t_sub.merge(
+#     df_s_sub, on='Encrypted Workstation Name', how='inner', indicator=True)
+# df_workShared.set_index('Encrypted Workstation Name')
+# df_workShared.to_excel('./data/work_shared.xlsx')
 
 # version 1: If Agency ID does not equal at least 1 Tanium, add to excel
-df_idMismatch = df_workShared.iloc[0:0]
+# df_idMismatch = df_workShared.iloc[0:0]
 
-for index, row in df_workShared.iterrows():
-    print('This is the row:' + row['Encrypted Workstation Name'])
-    mistmatch = False
-    for col in idColumns:
-        if (row['Agency'] != row[col] and not pd.isnull(row[col])):
-            mismatch = True
-            if mismatch:
-                break
-    if mismatch:
-        df_idMismatch.append(df_workShared)
-        print('row appended')
-print('done loading')
 
-print(df_idMismatch)
-df_idMismatch.to_excel('./data/mismatched_usage.xlsx')
+# Do not iterate manually
+# for index, row in df_workShared.iterrows():
+#     print('This is the row:' + row['Encrypted Workstation Name'])
+#     mistmatch = False
+#     for col in idColumns:
+#         if (row['Agency'] != row[col] and not pd.isnull(row[col])):
+#             mismatch = True
+#             if mismatch:
+#                 break
+#     if mismatch:
+#         df_idMismatch.append(df_workShared)
+#         print('row appended')
+# print('done loading')
+
+# print(df_idMismatch)
+# df_idMismatch.to_excel('./data/mismatched_usage.xlsx')
 
 # version 2: If Agency ID does not equal at least 1 Tanium, add to excel
 # df_idMismatch = df_workShared.iloc[0:0]
@@ -70,16 +76,17 @@ df_idMismatch.to_excel('./data/mismatched_usage.xlsx')
 # df_idMismatch.to_excel('./data/mismatched_usage.xlsx')
 
 # REPORT 2: Joint Report (SCCM reported Agency ID == all Tanium reported Agency ID)
-# df_idShared = df_workShared
-# for index, row in df_workShared.iterrows():
-#     for col in idColumns:
-#         if ((df_workShared['Agency'] != df_workShared[col]).any()):
-#             df_idShared = df_idShared.drop(
-#                 row['Encrypted Workstation Name'])
-# print(df_idShared)
+# Merge dataframes on workstation and index with workstation
+
 
 # REPORT 3: Only Tanium Report (Workstation Name is only in Tanium)
 # Schema: Workstation Name, Operating System, Tanium Agency ID 1, …
+df_t_unique = df_t_sub.merge(df_s_sub, indicator=True, how='outer').query(
+    '_merge=="left_only"').drop('_merge', axis=1)
+df_t_unique.to_excel('./data/tanium_unique.xlsx')
 
 # REPORT 4: Only SCCM Report (Workstation Name is only in SCCM)
 # Schema: Workstation Name, OS, OS Version, SCCM Agency ID
+df_s_unique = df_s_sub.merge(df_t_sub, indicator=True, how='outer').query(
+    '_merge=="left_only"').drop('_merge', axis=1)
+df_s_unique.to_excel('./data/sccm_unique.xlsx')
