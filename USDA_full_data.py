8a48502ff93cb0ac24edd88706e28fedbc0e02ce
Luke Sorensen <lukie176mc@gmail.com>
2022-01-19 18:04:31 -0700

    Generate matching and mismatching reports

--- USDA_full_data.py
+++ USDA_full_data.py
@@ -5,13 +5,15 @@ import os
 
 os.makedirs('./data', exist_ok=True)
 
-# Read Tanium input data as DataFrame df
-print('Reading in Tanium and SCCM data')
+# Import Tanium and SCCM input data as DataFrames
+print('Importing Tanium and SCCM data')
 df_t = pd.read_excel('Tanium_data.xlsx')
 df_s = pd.read_excel('SCCM_data.xlsx')
 
+print('Cleaning and preparing data')
+
 # Indicate all columns where AgencyID may be located for Tanium
-idColumns = [
+id_columns = [
     'Asset - Custom Tags.2.1',
     'Asset - Custom Tags.2.2.1',
     'Asset - Custom Tags.2.2.2.1',
@@ -22,43 +24,47 @@ idColumns = [
     'Asset - Custom Tags.2.2.2.2.2.2.2.2.2.1'
 ]
 
-# Clean all Agency IDs from Tanium data
-for column in idColumns:
-    df_t[column] = df_t[column].str.replace("AgencyID-", "")
-
-# REPORT 1: Mismatch Report (SCCM reported Agency ID != at least 1 Tanium reported Agency ID)
-# Schema: Workstation Name, Operating System, SCCM Agency ID, Tanium Agency ID 1, Tanium Agency ID 2, …
+# Clean Agency IDs and Mission Areas from Tanium data
+for col in id_columns:
+    df_t[col] = df_t[col].str.replace('AgencyID-', '')
+    df_t.loc[df_t[col].str.startswith('MissionArea', na=True), col] = ''
 
 # Keep only relevant columns from Tanium and SCCM datasets
-tCols = ['Encrypted Workstation Name', 'Operating System']
-tCols.extend(idColumns)
-df_t = df_t[tCols].groupby('Encrypted Workstation Name').first()
+# Group by workstation so each workstation is present only once
+t_cols = ['Encrypted Workstation Name', 'Operating System']
+t_cols.extend(id_columns)
+df_t = df_t[t_cols].groupby('Encrypted Workstation Name').first()
 
-sCols = ['Encrypted Workstation Name', 'Agency', 'OS']
-df_s = df_s[sCols].groupby('Encrypted Workstation Name').first()
+s_cols = ['Encrypted Workstation Name', 'Agency', 'OS']
+df_s = df_s[s_cols].groupby('Encrypted Workstation Name').first()
 
-print('Exporting grouped data to Excel')
-df_t.to_excel('./data/df_t_grouped.xlsx')
-df_s.to_excel('./data/df_s_grouped.xlsx')
+# print('Exporting grouped data to Excel') # TODO: remove
+# df_t.to_excel('./data/df_t_grouped.xlsx')
+# df_s.to_excel('./data/df_s_grouped.xlsx')
 
 # Merge DataFrames on workstation
-print('Merging datasets and exporting to Excel')
-df_workShared = df_t.merge(df_s, how='inner', on='Encrypted Workstation Name')
-df_workShared.to_excel('./data/df_workShared.xlsx')
+print('Merging Tanium and SCCM data')
+df_joint = df_t.merge(df_s, how='inner', on='Encrypted Workstation Name')
+# df_joint.to_excel('./data/df_joint.xlsx')
 
-# print('Finding matching AgencyID classifications')
-# df_workShared['matching'] = True
-# for col in idColumns:
-#     df_workShared['matching'] = df_workShared.loc[
-#         df_workShared['matching']
-#         & (~df_workShared[col].str.startswith("AgencyID")
-#         | df_workShared[col].str.replace('AgencyID-', '') == df_workShared['Agency'])
-#         ]
-# print('Exporting to Excel')
-# df_workShared.to_excel('./data/matching_indicated.xlsx')
+# 'Matching' indicates SCCM classification matches all Tanium classifications 
+print('Finding matching AgencyID classifications')
+df_joint['Matching'] = True
+for col in id_columns:
+    df_joint.loc[
+        (df_joint[col].str.isalpha()) &
+        (df_joint[col] != df_joint['Agency']), 'Matching'
+        ] = False
 
-# REPORT 2: Joint Report (SCCM reported Agency ID == all Tanium reported Agency ID)
-# Schema: Workstation Name, Operating System, SCCM Agency ID, Tanium Agency ID 1, Tanium Agency ID 2, …
+# REPORT 1: Matching Report (all classifications match)
+print('Exporting matching report to Excel')
+df_joint.loc[df_joint['Matching']] \
+    .to_excel('./data/matching_workstations.xlsx')
+
+# REPORT 2: Mismatch Report (at least one classification does not match)
+print('Exporting mismatching report to Excel')
+df_joint.loc[~df_joint['Matching']] \
+    .to_excel('./data/mismatching_workstations.xlsx')
 
 # REPORT 3: Only Tanium Report (Workstation Name is only in Tanium)
 # Schema: Workstation Name, Operating System, Tanium Agency ID 1, …
